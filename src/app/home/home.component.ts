43ecb9d145791785f40522290a7751b6a1e0b3e9
Marcin Banaszak <marcin.banaszak9@gmail.com>
2017-07-16 00:59:15 +0100

    add header to account route

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onSignOut() {
+    this.authService.logout();
+    this.router.navigate(['login']);
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home.routes';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 import { HomeComponent } from './home.component';
 import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [HomeComponent, AccountComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, MaterialModule, BrowserAnimationsModule],
   exports: [HomeComponent]
 })
 export class HomeModule {}
